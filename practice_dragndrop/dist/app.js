98400810c6f53590947024ee55f130c050527977
Clare <chblackburne@gmail.com>
2020-01-29 16:05:23 +0000

    add getter

--- practice_dragndrop/dist/app.js
+++ practice_dragndrop/dist/app.js
@@ -112,10 +112,16 @@ class ProjectItem extends Component {
         this.configure();
         this.renderContent();
     }
+    get persons() {
+        if (this.project.people === 1) {
+            return '1 person';
+        }
+        return `${this.project.people} persons`;
+    }
     configure() { }
     renderContent() {
         this.element.querySelector('h2').textContent = this.project.title;
-        this.element.querySelector('h3').textContent = this.project.people.toString();
+        this.element.querySelector('h3').textContent = this.project.people.toString() + ' assigned';
         this.element.querySelector('p').textContent = this.project.description;
     }
 }
